519adff6c9882f9bfb8cb1175dff87d4d965a1da
ranma2913 <4295880+ranma2913@users.noreply.github.com>
2023-12-13 16:11:16 -0600

    :sparkles: Initial Creation

---
+++ src/main/java/com/ranma2913/echoapi/EchoApiApplication.java
@@ -0,0 +1,12 @@
+package com.ranma2913.echoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EchoApiApplication.class, args);
+  }
+}
---
+++ src/main/java/com/ranma2913/echoapi/RestApi.java
@@ -0,0 +1,41 @@
+package com.ranma2913.echoapi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/echo")
+public class RestApi {
+  @GetMapping
+  @ResponseStatus(code = HttpStatus.OK)
+  public void get(@RequestHeader HttpHeaders headers) {
+    log.info("Endpoint called with\n  headers={}", headers);
+  }
+
+  @PutMapping
+  @ResponseStatus(code = HttpStatus.OK)
+  public void put(@RequestBody Object body, @RequestHeader HttpHeaders headers) {
+    log.info("Endpoint called with\n  body={}\n  headers={}", body, headers);
+  }
+
+  @PatchMapping
+  @ResponseStatus(code = HttpStatus.OK)
+  public void patch(@RequestBody Object body, @RequestHeader HttpHeaders headers) {
+    log.info("Endpoint called with\n  body={}\n  headers={}", body, headers);
+  }
+
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public void post(@RequestBody Object body, @RequestHeader HttpHeaders headers) {
+    log.info("Endpoint called with\n  body={}\n  headers={}", body, headers);
+  }
+
+  @DeleteMapping
+  @ResponseStatus(code = HttpStatus.NO_CONTENT)
+  public void delete(@RequestBody Object body, @RequestHeader HttpHeaders headers) {
+    log.info("Endpoint called with\n  body={}\n  headers={}", body, headers);
+  }
+}
